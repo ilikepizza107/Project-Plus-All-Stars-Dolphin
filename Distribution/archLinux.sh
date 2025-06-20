86f118cb3791b3813a840d13365f680870088e1c
Paul Joseph Canavan <ssbmotobug@gmail.com>
2025-06-20 11:40:37 -0400

    More script adjustments

--- Distribution/archLinux.sh
+++ Distribution/archLinux.sh
@@ -8,14 +8,14 @@ commit_hash=$(git rev-parse --short=7 HEAD)
 pkgver="${commit_hash}"
 pkgrel=1
 arch="x86_64"
-pkgdesc="A GameCube and Wii emulator optimized for Mario Party Netplay"
+pkgdesc="A GameCube and Wii emulator optimized for Project Plus Netplay"
 depends=("glibc" "sdl2" "ffmpeg" "qt6-base" "libx11" "libxi" "libxrandr" "zlib" "libevdev")
 license=("GPL2")
 
 # Convert generated archive to Arch format
 mkdir -p "${pkgname}-${pkgver}-${arch}"
-tar -xzf DolphinMPN-*.tar.gz -C "${pkgname}-${pkgver}-${arch}"
-mv "${pkgname}-${pkgver}-${arch}/DolphinMPN-archLinux" "${pkgname}-${pkgver}-${arch}"
+tar -xzf Project-Plus-Dolphin-*.tar.gz -C "${pkgname}-${pkgver}-${arch}"
+mv "${pkgname}-${pkgver}-${arch}/Project-Plus-Dolphin-archLinux" "${pkgname}-${pkgver}-${arch}"
 # Create .PKGINFO file
 cat <<EOF > "${pkgname}-${pkgver}-${arch}/.PKGINFO"
 pkgname=${pkgname}
@@ -27,4 +27,4 @@ depends=(${depends[@]})
 EOF
 
 # Compress into a valid Arch package format
-tar -cJf "DolphinMPN-archLinux.pkg.tar.zst" -C "${pkgname}-${pkgver}-${arch}" .
+tar -cJf "Project-Plus-Dolphin-archLinux.pkg.tar.zst" -C "${pkgname}-${pkgver}-${arch}" .
