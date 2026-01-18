c9c846d5a8623dd965a10bc0b3e6be9bee31897a
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2026-01-18 09:05:49 -0600

    Theme change and saves

--- Source/Core/Common/CommonPaths.h
+++ Source/Core/Common/CommonPaths.h
@@ -102,7 +102,7 @@
 #define SHADERCACHE_LEGACY_DIR "ShaderCache"
 
 // The theme directory used by default
-#define DEFAULT_THEME_DIR "Clean"
+#define DEFAULT_THEME_DIR "Clean Pink"
 
 // Filenames
 // Files in the directory returned by GetUserPath(D_CONFIG_IDX)
